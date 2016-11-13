22acd1d23cb01a7c22032c4b98fd48f2b517078f
Oliver Eisenhut <oliver.eisenhut@gmail.com>
2016-11-13 18:20:03 +0100

    Render nested components

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import Counter from './Counter';
 import './App.css';
 
 class App extends Component {
-  
+
   state = {
     count: 0
   };
-  
+
   onClick = (event) => {
     this.setState({count: this.state.count + 1});
   }
-  
-  
+
+
   render() {
     return (
       <div className="App">
         <Header title="Where Are You"/>
         <main className="Content">
           <Counter onClick={this.onClick} count={this.state.count}/>
+          {this.props.children}
         </main>
       </div>
     );
