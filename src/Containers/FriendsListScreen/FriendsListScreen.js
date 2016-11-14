a7e45f27ac78bf7bc2af0ecd06d96d3a0058b746
michaelmattle <michaelmattle@live.com>
2016-11-14 13:46:22 +0100

    Added FriendsListScreen

---
+++ src/Containers/FriendsListScreen/FriendsListScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TrackerMap from '../../Components/GoogleMaps/TrackerMap';
+import './FriendsListScreen.css';
+
+class FriendsListScreen extends Component {
+
+  render() {
+		return (
+			<div style={{height: '400px' }}>        
+			
+			</div>
+    );
+  }
+}
+
+export default FriendsListScreen;
