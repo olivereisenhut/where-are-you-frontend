fd6616c544f401a61e7de2e7f04b43eedf9a3724
dariogartmann <mail@dariogartmann.ch>
2016-11-14 11:01:52 +0100

    added missing packages and simple geolocation

--- src/Components/GoogleMaps/TrackerMap.js
+++ src/Components/GoogleMaps/TrackerMap.js
@@ -1,4 +1,9 @@
+import canUseDOM from "can-use-dom";
+import raf from "raf";
+
 import React, { Component } from 'react';
+
+
 import { withGoogleMap, GoogleMap } from "react-google-maps";
 import './TrackerMap.css';
 
@@ -6,22 +11,83 @@ const SimpleMapExampleGoogleMap = withGoogleMap(props => (
   <GoogleMap
     defaultZoom={14}
     defaultCenter={{ lat: 47.0478519, lng: 9.440610500000048 }}
+		center={props.center}
   />
 ));
 
+const geolocation = (
+  canUseDOM && navigator.geolocation ?
+  navigator.geolocation : 
+  ({
+    getCurrentPosition(success, failure) {
+      failure(`Your browser doesn't support geolocation.`);
+    },
+  })
+);
 
 class TrackerMap extends Component {
 
+	state = {
+    center: null,
+    content: null,
+    radius: 6000,
+  };
+
+  isUnmounted = false;
+
+ componentDidMount() {
+    const tick = () => {
+      if (this.isUnmounted) {
+        return;
+      }
+      this.setState({ radius: Math.max(this.state.radius - 20, 0) });
+
+      if (this.state.radius > 200) {
+        raf(tick);
+      }
+    };
+    geolocation.getCurrentPosition((position) => {
+      if (this.isUnmounted) {
+        return;
+      }
+      this.setState({
+        center: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        },
+        content: `Location found using HTML5.`,
+      });
+
+      raf(tick);
+    }, (reason) => {
+      if (this.isUnmounted) {
+        return;
+      }
+      this.setState({
+        center: {
+          lat: 60,
+          lng: 105,
+        },
+        content: `Error: The Geolocation service failed (${reason}).`,
+      });
+    });
+  }
+
+
+  componentWillUnmount() {
+    this.isUnmounted = true;
+  }
 
   render() {
     return (
-       <SimpleMapExampleGoogleMap
-        containerElement={
+		 	<SimpleMapExampleGoogleMap
+				containerElement={
           <div style={{ height: `100%` }} />
         }
-        mapElement={
+				mapElement={
           <div style={{ height: `100%` }} />
         }
+				center={this.state.center}
       />
     );
   }
