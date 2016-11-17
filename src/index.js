057ca4d279dd38ccbcd4a09b12b3243eea36ac54
michaelmattle <michaelmattle@live.com>
2016-11-17 14:49:24 +0100

    added redirect to login, if not logged in

--- src/index.js
+++ src/index.js
@@ -10,13 +10,26 @@ import FriendsListScreen from './Containers/FriendsListScreen/FriendsListScreen'
 
 import './index.css';
 
+/*function requireAuth() {
+	let found = hashHistory.find((historyItem) => {
+		if (historyItem == '/login') {
+			console.log(historyItem);
+			return true;
+		}
+	});
+	
+	if (!found) {
+		console.log('You are not logged in');
+	}
+}*/
+
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App} >
       <Route path="/login" component={LoginScreen} />
-			<Route path="/track" component={MapScreen} />
-			<Route path="/friends" component={FriendsListScreen} />
-			<Route path="/friends/add" component={AddFriendScreen} />
+			<Route path="/track" component={MapScreen} /*onEnter={requireAuth}*/ />
+			<Route path="/friends" component={FriendsListScreen} /*onEnter={requireAuth}*/ />
+			<Route path="/friends/add" component={AddFriendScreen} /*onEnter={requireAuth}*/ />
 			</Route>
   </Router>
   ), document.getElementById('root')
