f11672a8a160100f334a45ef9a13e062d68b25a9
Oliver Eisenhut <oliver.eisenhut@gmail.com>
2016-11-17 21:29:28 +0100

    style login screen

--- src/Containers/LoginScreen/LoginScreen.js
+++ src/Containers/LoginScreen/LoginScreen.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
 
+import FontAwesome from 'react-fontawesome';
 import './LoginScreen.css';
+import * as GoogleButton from "./GoogleButton.css";
 
 
 const responseGoogle = (response) => {
@@ -19,14 +21,19 @@ class LoginScreen extends Component {
     return (
         <div className="Login-container">
             <main className="Content">
-               <GoogleLogin
-                  onClick={this.onClick}
-                    clientId={'135317923400-spd82dqbrhcbq5k6nvskhdodgtb34ana.apps.googleusercontent.com'}
-                    onSuccess={this.props.handleLogin}
-                    onFailure={responseGoogle}
-                    offline={false}
-                    style={{}}>
-                </GoogleLogin>
+              <div className="login">
+                <div className="google-login">
+                  <FontAwesome name='google-plus' />
+                  <GoogleLogin
+                      onClick={this.onClick}
+                      clientId={'135317923400-spd82dqbrhcbq5k6nvskhdodgtb34ana.apps.googleusercontent.com'}
+                      onSuccess={this.props.handleLogin}
+                      onFailure={responseGoogle}
+                      offline={false}
+                      style={GoogleButton}>
+                  </GoogleLogin>
+                </div>
+              </div>
             </main>
         </div>
     );
