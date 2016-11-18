72aa02bef01e118c4f84ef002f533162bb25be7a
Oliver Eisenhut <oliver.eisenhut@gmail.com>
2016-11-18 13:18:01 +0100

    Redirect root to friend list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, Redirect } from 'react-router';
 import App from './Containers/App/App';
 import LoginScreen from './Containers/LoginScreen/LoginScreen';
 import MapScreen from './Containers/MapScreen/MapScreen';
@@ -11,6 +11,7 @@ import './index.css';
 
 ReactDOM.render((
   <Router history={hashHistory}>
+    <Redirect from="/" to="/friends" />
     <Route path="/" component={App} >
       <Route path="/login" component={LoginScreen} />
 			<Route path="/track/:userId" component={MapScreen} />
